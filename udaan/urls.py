05e05fb67f123bb3095676c86c5e67b4ac30bf41
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2023-03-21 12:24:08 +0530

    Update urls.py

--- udaan/urls.py
+++ udaan/urls.py
@@ -18,13 +18,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from udaan_backend.views import *
-from django.contrib.staticfiles import views
-from django.urls import re_path
+# from django.contrib.staticfiles import views
+# from django.urls import re_path
 
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(/static/(?P<path>.*)$', views.serve),
-    ]
+# if settings.DEBUG:
+#     urlpatterns += [
+#         re_path(/static/(?P<path>.*)$', views.serve),
+#     ]
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('msg/',ReactView.as_view(),name="msg"),
